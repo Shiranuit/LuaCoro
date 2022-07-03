7d8e68cbabe39fa647b8c9e70313a96e92fd1765
Shiranuit <shiranui007@gmail.com>
2022-07-03 19:53:22 +0200

    Add promise support, refactor coro code to make it cleaner

--- coro.lua
+++ coro.lua
@@ -1,162 +1,288 @@
-local coroutines = {}
+local promises = {}
+local promiseId = 1
+local promise = {}
+
+function promise.isPromise(obj)
+  return obj and type(obj) == 'table' and obj.__type == 'promise'
+end
+
+function promise.status(obj)
+  return obj.__status
+end
+
+function promise.resolve(obj, ...)
+  obj.__status = 'resolved'
+  obj.__value = { ... }
+end
+
+function promise.error(obj, err)
+  obj.__status = 'errored'
+  obj.__value = err
+end
+
+function promise.await(obj)
+  while obj.__status == 'pending' do
+    coroutine.yield()
+  end
+
+  if obj.__status == 'resolved' then
+    if promise.isPromise(obj.__value[1]) then
+      return promise.await(obj.__value[1])
+    end
+    return table.unpack(obj.__value)
+  else
+    error(obj.__value)
+  end
+end
+
+function promise.chain(obj, func)
+  assert(type(func) == 'function', 'function expected')
+
+  local newPromise = promise.new(func, false)
+  newPromise.__trigger = obj
+  newPromise.__asyncMethod = true
+  return newPromise
+end
+
+function promise.catch(obj, func)
+  assert(type(func) == 'function', 'function expected')
+
+  local newPromise = promise.new(func, false)
+  newPromise.__trigger = obj
+  newPromise.__asyncMethod = true
+  newPromise.__catch = true
+  return newPromise
+end
+
+function promise.isErrored(obj)
+  return obj.__status == 'errored'
+end
+
+function promise.isResolved(obj)
+  return obj.__status == 'resolved'
+end
+
+function promise.isPending(obj)
+  return obj.__status == 'pending'
+end
+
+function promise.new(func, run)
+  assert(type(func) == 'function', 'Exepected a function')
+
+  local co = coroutine.create(func)
+
+  local newPromise = {
+    __coroutine = co,
+    __type = 'promise',
+    __status = 'pending',
+    __catch = false,
+    __asyncMethod = false,
+    __id = promiseId,
+    status = promise.status,
+    resolve = promise.resolve,
+    error = promise.error,
+    await = promise.await,
+    chain = promise.chain,
+    catch = promise.catch,
+    isErrored = promise.isErrored,
+    isResolved = promise.isResolved,
+    isPending = promise.isPending,
+  }
+
+  promiseId = promiseId + 1
+
+  promises[#promises + 1] = newPromise
+
+  if run then
+    local resolve = function(...)
+      promise.resolve(newPromise, ...)
+    end
+    local error = function(...)
+      promise.error(newPromise, ...)
+    end
+
+    local out = { coroutine.resume(co, resolve, error) }
+
+    local success = out[1]
+
+    if coroutine.status(newPromise.__coroutine) == 'dead' and success == false then
+      promise.error(newPromise, out[1])
+    end
+  end
+
+  return newPromise
+end
 
 local function range(start, stop, step)
     local step = step or 1
-    for i=start, stop, step do
-        coroutine.yield(i)
+    for i = start, stop, step do
+      coroutine.yield(i)
     end
 end
 
 local function _forward(func)
-    return function(...)
-        local args = {...}
-        local co = coroutine.create(func)
-        return function()
-            if coroutine.status(co) ~= 'dead' then
-                local result = {coroutine.resume(co, table.unpack(args))}
-                coroutine.yield()
-                return select(2, table.unpack(result))
-            end
-        end
+  return function(...)
+    local args = { ... }
+    local co = coroutine.create(func)
+    return function()
+      if coroutine.status(co) ~= 'dead' then
+        local result = { coroutine.resume(co, table.unpack(args)) }
+        coroutine.yield()
+        return select(2, table.unpack(result))
+      end
     end
+  end
 end
 
-local function run(func, ...)
-    local mainCoro = coroutine.create(func)
-    local task = {
-        type='task',
-        co=mainCoro,
-        args={...},
-    }
+local function run(func)
+  local mainPromise = promise.new(func, false)
+  mainPromise.__asyncMethod = true
 
-    coroutines[#coroutines+1] = task
+  local i = 1
+  while #promises > 0 do
+    local prom = promises[i]
+    if coroutine.status(prom.__coroutine) == 'dead' or not promise.isPending(prom) then
+      table.remove(promises, i)
+    elseif not prom.__trigger or not promise.isPending(prom.__trigger) then
 
-    local out = {coroutine.resume(task.co, table.unpack(task.args))}
-    if coroutine.status(task.co) == 'dead' then
-        if out[1] then
-            task.result = {select(2, table.unpack(out))}
+      local skip = false
+      if prom.__trigger and prom.__catch and promise.isResolved(prom.__trigger) then
+        prom.__status = 'resolved'
+        prom.__value = prom.__trigger.__value
+        skip = true
+        table.remove(promises, i)
+      elseif prom.__trigger and prom.__catch == false and promise.isErrored(prom.__trigger) then
+        prom.__status = 'errored'
+        prom.__value = { prom.__trigger.__value }
+        skip = true
+        table.remove(promises, i)
+      end
+
+      if not skip then
+        local out = { coroutine.resume(prom.__coroutine, table.unpack(prom.__trigger and prom.__trigger.__value or {})) }
+
+        if coroutine.status(prom.__coroutine) == 'dead' then
+          if prom.__asyncMethod or out[1] == false then
+            prom.__value = out[1] and { select(2, table.unpack(out)) } or out[2]
+            prom.__status = out[1] and 'resolved' or 'errored'
+          end
+          table.remove(promises, i)
         else
-            task.error = out[2]
+          i = i + 1
         end
+      end
+    else
+      i = i + 1
     end
-    
-    local i = 1
-    while #coroutines > 0 do
-        local task = coroutines[i]
-        if coroutine.status(task.co) == 'dead' then
-            table.remove(coroutines, i)
-        else
-            local out = {coroutine.resume(task.co)}
-            if coroutine.status(task.co) == 'dead' then
-                if out[1] then
-                    task.result = {select(2, table.unpack(out))}
-                else
-                    task.error = out[2]
-                end
-                table.remove(coroutines, i)
-            else
-                i = i + 1
-            end
-        end
-        if i > #coroutines then
-            i = 1
-        end
+    if i > #promises then
+      i = 1
     end
+  end
+  
+  if mainPromise.__status == 'resolved' then
+    return table.unpack(mainPromise.__value)
+  else
+    error(mainPromise.__value)
+  end
 end
 
 local function async(func)
-    return function(...)
-        local co = coroutine.create(func)
-        local task = {
-            type='task',
-            co=co,
-            args={...},
-        }
-        coroutines[#coroutines + 1] = task
-        local out = {coroutine.resume(task.co, table.unpack(task.args))}
-        if coroutine.status(task.co) == 'dead' then
-            if out[1] then
-                task.result = {select(2, table.unpack(out))}
-            else
-                task.error = out[2]
-            end
-        end
-        return task
+  return function(...)
+    local newPromise = promise.new(func, false)
+    newPromise.__asyncMethod = true
+
+    local out = { coroutine.resume(newPromise.__coroutine, ...) }
+
+    local success = out[1]
+    table.remove(out, 1)
+
+    if coroutine.status(newPromise.__coroutine) == 'dead' then
+      newPromise.__value = success and out or out[1]
+      newPromise.__status = success and 'resolved' or 'errored'
     end
+
+    return newPromise
+  end
 end
 
-local function await(obj, ...)
-    if type(obj) == 'table' and obj.type == 'task' then
-        while coroutine.status(obj.co) ~= 'dead' do
-            coroutine.yield()
-        end
-        if obj.error then
-            error(obj.error)
-        end
-        return table.unpack(obj.result)
-    elseif type(obj) == 'function' then
-        local task = coro.async(obj)()
-        while coroutine.status(task.co) ~= 'dead' do
-            coroutine.yield()
-        end
-        if task.error then
-            error(task.error)
-        end
-        return table.unpack(task.result)
-    else
-        return coroutine.yield(obj)
-    end
+local function await(obj)
+  if promise.isPromise(obj) then
+    return promise.await(obj)
+  end
+  return coroutine.yield(obj)
 end
 
-local function waitForAll(tasks)
-    local results = {}
-    for i=1, #tasks do
-        results[i] = await(tasks[i])
+local function all(promises)
+  return promise.new(function(res, rej)
+    local promisesResults = {}
+    for i = 1, #promises do
+      promisesResults[i] = await(promises[i])
     end
-    return results
+    res(promisesResults)
+  end, true)
 end
 
-local function waitForAny(tasks)
-    local running = false
+local function waitForAll(promises)
+  return await(all(promises))
+end
 
-    for i=1, #tasks do
-        local task = tasks[i]
-        if type(task) == 'table' and task.type == 'task' then
-            running = true
-            break
-        end
+local function any(promises)
+  return promise.new(function(res, err)
+    local filteredPromises = {}
+
+    for i = 1, #promises do
+      local obj = promises[i]
+      if promise.isPromise(obj) then
+        filteredPromises[#filteredPromises + 1] = obj
+      end
     end
 
-    local finalTask = {}
+    local finishedProm = {}
 
     local i = 1
-    while running do
-        local task = tasks[i]
-        if type(task) == 'table' and task.type == 'task' then
-            if coroutine.status(task.co) ~= 'dead' then
-                coroutine.yield()
-            else
-                finalTask = task
-                running = false
-            end
-        end
-        i = i + 1
-        if i > #tasks then
-            i = 1
-        end
+    while true do
+      local prom = filteredPromises[i]
+      if promise.isPending(prom) then
+        coroutine.yield()
+      else
+        finishedProm = prom
+        break
+      end
+      i = i + 1
+      if i > #filteredPromises then
+        i = 1
+      end
+    end
+    
+    if finishedProm.__status == 'resolved' then
+      res(table.unpack(finishedProm.__value))
+    else
+      err(finishedProm.__value)
     end
 
-    if task.error then
-        error(task.error)
-    end
-    return table.unpack(task.result)
+  end, true)
+end
+
+local function waitForAny(promises)
+  return await(any(promises))
 end
 
 return {
-    range=_forward(range),
-    run=run,
-    async=async,
-    await=await,
-    waitForAll=waitForAll,
-    waitForAny=waitForAny,
+  range = _forward(range),
+  run = run,
+  async = async,
+  await = await,
+  waitForAll = waitForAll,
+  waitForAny = waitForAny,
+  all = all,
+  any = any,
+  promise = {
+    new = function(func)
+      return promise.new(func, true)
+    end,
+    isErrored = promise.isErrored,
+    isResolved = promise.isResolved,
+    isPending = promise.isPending,
+    isPromise = promise.isPromise,
+  }
 }
